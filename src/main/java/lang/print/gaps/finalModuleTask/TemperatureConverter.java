2e85f161fa0c2691cdd0c529086ec49923949e6d
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 17:05:28 +0300

    Fix task

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,6 +2,5 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        System.out.println((temperatureCelsius * (9.0/5)) + 32);
     }
 }
